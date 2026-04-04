72a79f7e6519580498f80388288d26542e91eeba
Ammar Ahmad <se.ammarahmad@gmail.com>
2026-04-04 21:37:48 +0500

    start

--- lib/core/utils/loader.dart
+++ lib/core/utils/loader.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
     @override
     Widget build(BuildContext context) {
       return Center(
+        child: CircularProgressIndicator.adaptive(),
       );
     }
   }
